641b7a5ca60ee804d6d103f1e5dc20d66be77c3c
kalandadze <luka.kalandadze1980@gmail.com>
2024-12-25 15:39:26 +0400

    v 0.1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
-import model.ArithmeticOperation;
-import service.ArithmeticOperationCalculator;
-import service.Interpreter;
-
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-        String op="5+10*2/4-9/3+7%4+21";
-        System.out.println(ArithmeticOperationCalculator.calculate(op,new HashMap<>()));
     }
 }
