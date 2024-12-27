734333f7649009348eebb8adf893d07e921ca6c6
kalandadze <luka.kalandadze1980@gmail.com>
2024-12-27 15:37:51 +0400

    v 1.0 final release

--- src/service/BooleanExpressionCalculator.java
+++ src/service/BooleanExpressionCalculator.java
@@ -3,12 +3,13 @@ package service;
 import util.Parser;
 import util.Tokenizer;
 
-import java.util.Scanner;
-
 public class BooleanExpressionCalculator {
 
     // Method to evaluate a boolean expression
     public static boolean evaluate(String expression) throws Exception {
+        expression = expression.replaceAll("true","1>0");
+        expression = expression.replaceAll("false","1<0");
+
         // Create a Tokenizer instance to tokenize the expression
         Tokenizer tokenizer = new Tokenizer(expression);
 
